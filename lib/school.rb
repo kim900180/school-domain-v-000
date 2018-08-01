c0cae883940739ed425250523ceb0e86a8f223b6
Jae Yong Kim <kim900180@gmail.com>
2018-08-01 18:28:03 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,21 @@
 # code here!
+class School
+  attr_accessor
+  attr_reader :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    @roster.keys.each do |key|
+      if key == grade
+        @roster[grade] << name
+      else
+        @roster[grade] = []
+        @roster[grade] << name
+      end
+    end
+  end
+end
